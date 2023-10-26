629387c8b8d6ea9b9e9be59d8b20f4b88975fba7
Kilian Maigrot <kilian.maigrot@gmail.com>
2023-10-26 11:38:12 +0200

    Ajout de la searchbar dans la liste des recettes

--- src/app/tab1/list-recette/list-recette.component.ts
+++ src/app/tab1/list-recette/list-recette.component.ts
@@ -8,11 +8,22 @@ import { ReloadListsService } from 'src/app/services/reloadLists/reload-lists.se
 })
 
 export class ListRecetteComponent  implements OnInit {
+
+  public results: any[]= [];
+
   
   constructor(public listService: ReloadListsService) { }
   
   ngOnInit(): void {    
-    this.listService.loadRecipes()
+    this.listService.loadRecipes()    
+    this.results = this.listService.recipesList
+    console.log(this.listService.recipesList);
+    
+  }
+
+  handleInput(event: any) {
+    const query = event.target.value.toLowerCase();    
+    this.results = this.listService.recipesList.filter((recipe) => recipe.name.toLowerCase().indexOf(query) > -1);
   }
    
 }
